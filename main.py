10c7cf92220945b919e833eff7e82dbbd746cca5
Dixin Fan <dishan@mail.ustc.edu.cn>
2022-01-24 15:33:00 +0800

    predict height successfully

--- main.py
+++ main.py
@@ -18,8 +18,16 @@ while True:
     ret, img_right = cap_right.read()
     depth, scale_y_left = caculate_params(img_left, img_right)
 
-    height, count_height, sum_height = caculate_height(depth, scale_y_left, height, count_height, sum_height)
-    distance = stablelize_depth(distance, depth)
+    # height, count_height, sum_height = caculate_height(depth, scale_y_left, height, count_height, sum_height)
+
+    # distance = stablelize_depth(distance, depth)
+    distance =  round(depth, 0)
+    if abs(distance - 180) <= 1:
+    # if distance == 160.0:
+        print(scale_y_left)
+        # distance = scale_y_left
+    # else:
+    #     distance = 0
 
     cv2.putText(img_left, "height: " + str(height), (50, 50), font, 1.2, color, 3)
     cv2.putText(img_right, "distance: " + str(distance), (50, 50), font, 1.2, color, 3)
